95b43ef89cd0f6f1992a65fe2d42b8e0393b9822
fxlipecdr <fxlipecdr@gmail.com>
2024-03-24 17:48:22 -0300

    projeto entregue

---
+++ escrevendo_classes_jogo.js.js
@@ -0,0 +1,25 @@
+class heroProperties {
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar(){
+        if (this.tipo == "mago"){
+            console.log(`o ${this.tipo} atacou usando magia`);
+        } else if (this.tipo == "guerreiro"){
+            console.log(`o ${this.tipo} atacou usando a espada`);
+        } else if (this.tipo == "monge"){
+            console.log(`o ${this.tipo} atacou usando artes marciais`);
+        } else if (this.tipo == "ninja"){
+            console.log(`o ${this.tipo} atacou usando shuriken`);
+        }
+    }
+}
+
+let hero = new heroProperties("Felipe","25","guerreiro")
+//let hero = new heroProperties("Joao","26","mago")
+//let hero = new heroProperties("Thiago","27","ninja")
+//let hero = new heroProperties("Luis","28","guerreiro")
+hero.atacar()
